1a0e94cdff6ca2525d69c07b8e1bd02572d3b439
glapul <michal.glapa@wp.pl>
2013-09-17 15:48:43 +0200

    template added

---
+++ template.cpp
@@ -0,0 +1,39 @@
+//Michał Glapa
+#include<cstdio>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<cstdlib>
+#include<time.h>
+#include<string>
+#include<stack>
+#include<cmath>
+#include<iostream>
+#include<cstring>
+#include<tr1/unordered_set>
+#include<tr1/unordered_map>
+using namespace std;
+using namespace tr1;
+#define FOR(i,j,k) for(int i=j;i<k;i++)
+#define FORD(i,j,k) for(int i=j;i>=k;i--)
+#define ll long long
+//Make sure no overflow problems
+#define int long long
+#define pii  pair<int, int>
+#define vi vector<int >
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+#define VAR(i,n) __typeof(n) i = (n)
+#define FOREACH(i,c) for(VAR(i,(c).begin());i!=(c).end();i++)
+#define FORDEACH(i,c) for( VAR(i,(c).rbegin(),i!=(c).rend();i++)
+#define REP(i,n) FOR(i,0,n)
+#define ld long double
+const int INF = 1000000009;
+const long long INFLL = (ll)INF * (ll)INF;
+const ld EPS = 10e-9;
+///////////////////////////////////////////////////////////////
+
